ff5110f25c25593f87a6ab7dde0582ea8be44af5
Aleksandr <for.melnik.dev@gmail.com>
2022-09-22 18:34:36 +0300

    eddit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <convertBlock />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import convertBlock from './components/convertBlock.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    convertBlock
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/convertBlock.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="block">
+    <ul class="currencies">
+      {defaultCurrencies.map((cur) => (
+      <li
+          @click="onChangeCurrency"
+          :class="currency ? 'active' : ''">
+        {cur}
+      </li>
+      ))}
+      <li>
+        <svg height="50px" viewBox="0 0 50 50" width="50px">
+          <rect fill="none" height="50" width="50" />
+          <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
+        </svg>
+      </li>
+    </ul>
+    <input
+        @change="onChangeValue(e.target.value)"
+        value={value}
+        type="number"
+        placeholder={0}
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "convertBlock",
+  data() {
+    return {
+      currency: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
